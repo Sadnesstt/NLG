b87d93936b96ff7076badd38fc0abb4fa9c87cbc
Sadnesstt <dnskdud88@gmail.com>
2024-02-28 07:02:54 +0900

    2024-02-28

---
+++ simple_nmt/simple_nmt/trainer.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import torch
+from torch import optim
+import torch.nn.utils as torch_utils
+from torch.cuda.amp import autocast
+from torch.cuda.amp import GradScaler
+
+from ignite.engine import Engine
+from ignite.engine import Events
+from ignite.metrics import RunningAverage
+from ignite.contrib.handlers.tqdm_logger import ProgressBar
+
+from simple_nmt.utils import get_grad_norm, get_parameter_norm
+
+
+VERBOSE_SILENT = 0
+VERBOSE_EPOCH_WISE = 1
+VERBOSE_BATCH_WISE = 2
+
+class MaximumLikelihoodEstimationEngine(Engine):
+
+    def __init__(self, func, model, crit, optimizer, lr_scheduler, config):
+        self.model = model
+        self.crit = crit
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.config = config
+
+        super().__init__(func)
+
+        self.best_loss = np.inf
+        self.scaler = GradScaler()
+
+    @staticmethod
+    #@profil
+    def train(engine, mini_batch):
+        # You have to reset the gradients of all model parameters
+        # before to take another step in gradient descent.
+        
