d80e9a4c8667cbec0bac12b76271c49734741e8e
Sadnesstt <dnskdud88@gmail.com>
2024-02-29 06:57:12 +0900

    2024-02-29

--- simple_nmt/simple_nmt/trainer.py
+++ simple_nmt/simple_nmt/trainer.py
@@ -37,4 +37,65 @@ class MaximumLikelihoodEstimationEngine(Engine):
     def train(engine, mini_batch):
         # You have to reset the gradients of all model parameters
         # before to take another step in gradient descent.
+        engine.model.train()
+        if engine.state.iteration % engine.config.iteration_per_update == 1 or engine.config.iteration_per_update == 1:
+            if engine.state.iteration > 1:
+                engine.optimizer.zero_grad()
+
+        device = next(engine.model.parameters()).device
+        mini_batch.src = (mini_batch.src[0].to(device), mini_batch.src[1])
+        mini_batch.tgt = (mini_batch.tgt[0].to(device), mini_batch.tgt[1])
+
+
+        # Raw target variable has both BOS and EOS token.
+        # The output of sequence-to-sequence does not have BOS token.
+        # Thus, remove BOS token for reference.
+        x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+        # |x| = (batch_size, length)
+        # |y| = (batch_size, length)
+
+        with autocast(not engine.config.off_autocast):
+            # Take feed-forward
+            # Similar as before, the input of decoder does not have EOS token.
+            # Thus, remove EOS token for decoder input.
+            y_hat = engine.model(x, mini_batch.tgt[0][:, :-1])
+            # |y_hat| = (batch_size, length, output_size)
+
+            loss = engine.crit(
+                y_hat.contiguous().view(-1, y_hat.size(-1)),
+                y.contiguous().view(-1)
+            )
+            backward_target = loss.div(y.size(0)).div(engine.config.iteration_per_update)
+
+        if engine.config.gpu_id >= 0 and not engine.config.off_autocast:
+            engine.scaler.scale(backward_target).backward()
+        else:
+            backward_target.backward()
+
+        word_count = int(mini_batch.tgt[1].sum())
+        p_norm = float(get_parameter_norm(engine.model.parameters()))
+        g_norm = float(get_grad_norm(engine.model.parameters()))
+
+        if engine.state.iteration % engine.config.iteration_per_update == 0 and engine.state.iteration > 0:
+            # In order to avoid gradient exploding, we apply gradient clipping.
+            torch_utils.clip_grad_norm_(
+                engine.model.parameters(),
+                engine.config.max_grad_norm,
+            )
+            # Take a step of gradient descent.
+            if engine.config.gpu_id >= 0 and not engine.config.off_autocast:
+                # Use scaler instead of engine.optimizer.step() if using GPU.
+                engine.scaler.step(engine.optimizer)
+                engine.scaler.update()
+            else:
+                engine.optimizer.step()
         
+        loss = float(loss / word_count)
+        ppl = np.exp(loss)
+
+        return {
+            'loss' : loss,
+            'ppl' : ppl,
+            '|param|' : p_norm if not np.isnan(p_norm) and not np.isinf(p_norm) else 0.,
+            '|g_param|' : g_norm if not np.isnan(g_norm) and not np.isinf(g_norm) else 0.
+        }
